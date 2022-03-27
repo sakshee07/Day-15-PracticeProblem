4d4f25fbdc5322de2bfe608858dcb9923988643a
sakshee07 <sakshee1@gmail.com>
2022-03-27 23:58:16 +0530

    added

---
+++ src/com/bst/bl/BinaryNode.java
@@ -0,0 +1,20 @@
+package com.bst.bl;
+
+public class BinaryNode<K extends Comparable<K>> {
+    K key;
+    BinaryNode<K> left;
+    BinaryNode<K> right;
+
+    public BinaryNode(K key)
+    {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+
+    @Override
+    public String toString() {
+        return "BinaryNode{" + "key=" + key + ", "
+        		+ "left=" + left + ", right=" + right + '}';
+    }
+}
---
+++ src/com/bst/bl/BinarySearchTree.java
@@ -0,0 +1,55 @@
+package com.bst.bl;
+
+public class BinarySearchTree <K extends Comparable<K>>{
+    public static void main(String[] args) {
+        System.out.println("Welcome to BinarySearchTree program");
+        BinarySearchTree<Integer> binaryTree = new BinarySearchTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        
+        int size = binaryTree.getSize();
+        System.out.println(binaryTree);
+        System.out.println("size of binary tree:"+size);
+    }
+    private BinaryNode<K> root;
+
+    public void add(K key)
+    {
+        this.root = this.insertData(root,key);
+    }
+
+    public BinaryNode<K> insertData(BinaryNode<K> current, K key)
+    {
+        if(current ==  null)
+            return new BinaryNode<>(key);
+        int compareResult = key.compareTo(current.key);
+
+        if(compareResult==0)
+            return current;
+
+        if(compareResult<0)
+        {
+            current.left = insertData(current.left, key);
+        }
+        else
+        {
+            current.right = insertData(current.right, key);
+        }
+        return current;
+    }
+    public int getSize()
+    {
+        return this.getSizeBinary(root);
+    }
+    public int getSizeBinary(BinaryNode<K> current)
+    {
+        return current == null ? 0 : 1 + this.getSizeBinary(current.left)+this.getSizeBinary(current.right);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "MyBinary [root=" + root + "]";
+    }
+}
